888311e6823a3ab71c6a7ae9d79a96df6c779aea
martarodriguezhebles <marta-r@live.com>
2023-10-15 19:45:14 +0200

    fichero con los enunciados de los problemas

--- problemas_enunciados.R
+++ problemas_enunciados.R
@@ -1,1 +1,21 @@
 ### ENUNCIADOS PROBLEMAS 
+
+# Primer problema
+set.seed(123)
+datos=runif(25)
+datos
+tabla=crea.tablaX(datos,5,5)
+tabla 
+
+
+#SEGUNDO PROBLEMA 
+
+# Una empresa de venta de electrodomésticos desea contratar un proveedor para el próximo año,
+# de manera que le salga lo más rentable posible. todos los proveedores tienes un precio fijo 
+# inicial para un pedido de 1000 unidades, dependiendo de la marca, y dicho precio aumenta 
+# por cada 100 unidades extra que la empresa solicite fuera de plazo. 
+# Tiene tres opciones, el proveedor A le cobra 500 u.m.por el primer pedido, el B 450 u.m. 
+# y el C 700 u.m.. Si añade 100 unidades mas, el proveedor A le cobrara 200 u.m. de suplemento, 
+# el B 250 u.m. y el C 100 u.m.. Mientras que si añade otras 100 unidades mas el proveedor A 
+# le sumará 150 u.m, el B 200 u.m. y el C nada. ¿Cuál es la opción más rentable si espera muchas 
+# ventas este año? ¿Y si tiene una previsión negativa?
